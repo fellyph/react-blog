4c48b730756f9716096c6608ec69d68077885410
Fellyph Cintra <Cintra>
2023-06-18 20:13:20 +0100

    adding theme variables to styled header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,34 +6,27 @@ type HeaderProps = {
   subtitle: string;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-const StyledHeader = styled('header')` 
-  backgroundColor: '#282c34',
-  minHeight: '10vh',
-  display: 'flex',
-  flexDirection: 'column',
-  padding: '2rem',
-
-  '& h1': {
-    color: 'white',
-    fontSize: 'clamp(24px, 4vw, 48px)',
-    color: props.theme.colors.primary,
-  },
-
-  '& h2': {
-    color: 'white',
-    fontSize: 'clamp(16px, 2vw, 24px)',
-  },
+const StyledHeader = styled('header')`
+  background-color: ${(props) => props.theme.colors.backgroundDark};
+  min-height: 10vh;
+  padding: ${(props) => props.theme.spacing.lg};
 `;
 
 const StyleTitle = styled('h1')`
   color: ${(props) => props.theme.colors.primary};
+  fontsize: 'clamp(24px, 4vw, 48px)';
+`;
+
+const StyledSubtitle = styled('h2')`
+  color: ${(props) => props.theme.colors.secondary};
+  fontsize: 'clamp(16px, 2vw, 24px)';
 `;
 
 const Header: FC<HeaderProps> = ({ title, subtitle, ...restProps }) => {
   return (
     <StyledHeader {...restProps}>
       <StyleTitle>{title}</StyleTitle>
-      <h2>{subtitle}</h2>
+      <StyledSubtitle>{subtitle}</StyledSubtitle>
     </StyledHeader>
   );
 };
