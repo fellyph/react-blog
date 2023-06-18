84dac60730dd9bac5a2906be5c3132202007c166
Fellyph Cintra <Cintra>
2023-06-18 19:53:37 +0100

    applying prettier to api.ts

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,15 +1,15 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const postsApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://blog.fellyph.com.br/wp-json/wp/v2",
+    baseUrl: 'https://blog.fellyph.com.br/wp-json/wp/v2',
   }),
-  endpoints: (builder) => ({
+  endpoints: builder => ({
     getPosts: builder.query({
-      query: () => "/posts",
+      query: () => '/posts',
     }),
     getPages: builder.query({
-      query: () => "/pages",
+      query: () => '/pages',
     }),
   }),
 });
