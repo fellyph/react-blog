37ce2f29b9fc378eeadd0988dc6a5a9e58dece02
Fellyph Cintra <Cintra>
2023-06-18 19:45:35 +0100

    adding first markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import styled from '@emotion/styled';
+
+const StyledWrapper = styled('div')({
+  width: 'min(100% - 3rem, 100ch)',
+  marginInline: 'auto',
+  padding: '1rem',
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <StyledWrapper>
+      <Header title="Welcome to React" subtitle="with TypeScript" />
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos</div>
+      <Footer signature="Fellyph Cintra" />
+    </StyledWrapper>
+  );
 }
 
-export default App
+export default App;
