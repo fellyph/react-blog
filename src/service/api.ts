1fbf0170d05d9741fd8cfe73fae64bd8bae6f2e1
Fellyph Cintra <Cintra>
2023-06-18 11:45:29 +0100

    first commit

---
+++ src/service/api.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const postsApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://blog.fellyph.com.br/wp-json/wp/v2",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+    }),
+    getPages: builder.query({
+      query: () => "/pages",
+    }),
+  }),
+});
