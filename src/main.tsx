9885eeae4317464b3e9b3e175449b733df6f7c9b
Fellyph Cintra <Cintra>
2023-06-18 19:46:13 +0100

    adding theme for react application with emotion

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { ThemeProvider } from '@emotion/react';
+
+const theme = {
+  colors: {
+    text: ' #060404',
+    background: '#f6efef',
+    primary: '#b1d2b3',
+    secondary: '#ffffff',
+    accent: '#b3b1d2',
+  },
+  fonts: {
+    body: '"Inconsolata", monospace',
+    heading: '"Karla", sans-serif',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
