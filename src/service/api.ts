f8d0df8d954788645590e07c88bddf011da7ee79
Fellyph Cintra <Cintra>
2023-06-25 16:27:32 +0100

    adding new site info endpoint

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,23 +1,27 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Post } from '../types/Post';
+import { SiteInfo } from '../types/SiteInfo';
 
 type PostsResponse = Post[];
 
 export const contentApi = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://blog.fellyph.com.br/wp-json/wp/v2',
+    baseUrl: 'https://blog.fellyph.com.br/wp-json',
   }),
   endpoints: (builder) => ({
     getPosts: builder.query<PostsResponse, void>({
-      query: () => '/posts',
+      query: () => '/wp/v2/posts',
     }),
     getPost: builder.query<Post, number>({
-      query: (id) => `/posts/${id}`,
+      query: (id) => `/wp/v2/posts/${id}`,
     }),
     getPages: builder.query({
-      query: () => '/pages',
+      query: () => '/wp/v2/pages',
+    }),
+    getSiteInfo: builder.query<SiteInfo, void>({
+      query: () => '/',
     }),
   }),
 });
 
-export const { useGetPostsQuery, useGetPagesQuery, useGetPostQuery } = contentApi;
+export const { useGetPostsQuery, useGetPagesQuery, useGetPostQuery, useGetSiteInfoQuery } = contentApi;
