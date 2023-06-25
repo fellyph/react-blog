f1da7bdda3c24d1def520cbae9a6b16d8f61c3b4
Fellyph Cintra <Cintra>
2023-06-25 10:58:47 +0100

    loading fetched data

--- src/components/views/home/Home.tsx
+++ src/components/views/home/Home.tsx
@@ -1,34 +1,16 @@
 import React, { FC } from 'react';
-// import { useGetPostsQuery } from '../../../service/api';
-// import { Post } from '../../../types/Post';
+import { useGetPostsQuery } from '../../../service/api';
+import { Post } from '../../../types/Post';
 import PostCard from '../../card/PostCard';
 
 const Home: FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
-  // const { data: postData, isLoading: postDataIsLoading } = useGetPostsQuery();
-  // console.log(postData);
+  const { data: postData, isLoading: postDataIsLoading } = useGetPostsQuery();
+  console.log(postData);
 
   return (
     <div {...props}>
       <h1>Home</h1>
-      {/* postDataIsLoading ? <p>Loading</p> : postData.map((post: Post) => <p key={post.id}>{post.title.rendered}</p>) */}
-      <PostCard
-        postTitle={`Post Title 1`}
-        postCategory={['cat 1', 'cat 2']}
-        postDescription="This Post is cool!"
-        postDate="20-09-2023"
-      ></PostCard>
-      <PostCard
-        postTitle={`Post Title 2`}
-        postCategory={['cat 1', 'cat 2']}
-        postDescription="This Post is cool!"
-        postDate="20-09-2023"
-      ></PostCard>
-      <PostCard
-        postTitle={`Post Title 4`}
-        postCategory={['cat 1', 'cat 2', 'cat 3', 'cat 4']}
-        postDescription="This Post is cool!"
-        postDate="20-09-2023"
-      ></PostCard>
+      {postDataIsLoading ? <p>Loading</p> : postData.map((post: Post) => <PostCard key={post.id} post={post} />)}
     </div>
   );
 };
