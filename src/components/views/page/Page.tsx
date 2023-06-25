9575a80c7ea59f5254569c0902617eba7ff7ab35
Fellyph Cintra <Cintra>
2023-06-25 20:19:33 +0100

    loading page error

--- src/components/views/page/Page.tsx
+++ src/components/views/page/Page.tsx
@@ -1,10 +1,17 @@
 import React, { FC } from 'react';
+import { useGetPageQuery } from '../../../service/api';
+import { useParams } from 'react-router-dom';
+import PostCard from '../../shared/card/PostCard';
+import { Loader } from '../../shared/Atoms';
+import { Page } from '../../../types/Page';
 
 const Page: FC<React.HTMLAttributes<HTMLDivElement>> = (props) => {
+  const { id } = useParams();
+  const { data: pageData, isLoading: pageDataIsLoading } = useGetPageQuery(id);
+
   return (
     <div {...props}>
-      <h1>Page</h1>
-      <p>Page page content</p>
+      {pageDataIsLoading ? <Loader>Loading...</Loader> : <PostCard className="page" post={pageData as Page} isSingle />}
     </div>
   );
 };
