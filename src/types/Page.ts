10e86aecdcb4890e76dd6ab2eaea22922193ddec
Fellyph Cintra <Cintra>
2023-06-25 19:33:56 +0100

    adding page interface to interact with the rest api

---
+++ src/types/Page.ts
@@ -0,0 +1,23 @@
+export interface Page {
+  id: number;
+  date: string;
+  date_gmt: string;
+  guid: {
+    rendered: string;
+  };
+  modified: string;
+  modified_gmt: string;
+  slug: string;
+  status: string;
+  type: string;
+  link: string;
+  title: {
+    rendered: string;
+  };
+  content: {
+    rendered: string;
+  };
+  excerpt: {
+    rendered: string;
+  };
+}
