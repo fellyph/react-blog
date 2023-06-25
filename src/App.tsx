297aac6db324cb89727982de0b4ddceab2caeb32
Fellyph Cintra <Cintra>
2023-06-25 16:03:34 +0100

    moving styled Wrapper to Atoms file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-import styled from '@emotion/styled';
 import { Outlet } from 'react-router-dom';
-
-const StyledWrapper = styled('div')({
-  width: 'min(100% - 3rem, 100ch)',
-  marginInline: 'auto',
-  padding: '1rem',
-});
+import { StyledWrapper } from './components/shared/Atoms';
 
 function App() {
   return (
-    <StyledWrapper>
+    <>
       <Header title="Welcome to React" subtitle="with TypeScript" />
-      <Outlet />
+      <StyledWrapper>
+        <Outlet />
+      </StyledWrapper>
       <Footer signature="Fellyph Cintra" />
-    </StyledWrapper>
+    </>
   );
 }
 
